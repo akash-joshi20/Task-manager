a5a6b5191a1a7174bff07b9001e3ef61924701b2
Akash <akash.joshi0920@gmail.com>
2025-11-20 20:53:30 +0545

    view, add, edit, delete and mark complete task feature

--- tasks/views.py
+++ tasks/views.py
@@ -4,6 +4,37 @@ from .models import Task
 
 # Create your views here.
 
+
 def task_list(request):
     tasks = Task.objects.all().order_by('-id')
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.description = request.POST.get('description', '')
+        task.save()
+        return redirect('task_list')
+
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
+
+
+def toggle_task_complete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('task_list')
